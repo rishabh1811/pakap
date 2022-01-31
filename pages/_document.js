3df4615731107b4c71ba2e8e686c26a147a1ab1b
Rishabh Gupta <nameisrishabh@gmail.com>
2022-01-31 16:24:59 +0530

    cloudflare analytics

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
+                    <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "d124f81191974609a810853b41b54054"}' />
                 </Head>
                 <body>
                     <Main />
@@ -19,3 +20,5 @@ class MyDocument extends Document {
 }
 
 export default MyDocument
+
+
