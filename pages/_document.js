8526a26fb806a63a8ebef57cf464d60a1d12e244
Rishabh Gupta <nameisrishabh@gmail.com>
2022-02-12 12:15:14 +0530

    favicon added

--- pages/_document.js
+++ pages/_document.js
@@ -23,7 +23,7 @@ class MyDocument extends Document {
   }}>
   </script>
 
-
+                    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet" />
